5441baac47c54c9a73e881a9012dcad33b4a5a32
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2021-09-04 00:13:27 +0530

    Fix test for create user

--- src/main/java/com/codecafe/bookshop/user/CreateUserRequest.java
+++ src/main/java/com/codecafe/bookshop/user/CreateUserRequest.java
@@ -2,11 +2,13 @@ package com.codecafe.bookshop.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@EqualsAndHashCode
 public class CreateUserRequest {
     private final String email;
     private final String password;
